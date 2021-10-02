3ae61c01c72e561a5446e2ceaf8100449d83059c
Daniel Lopez <d.lopez.1740@gmail.com>
2021-10-02 17:17:03 -0300

    40. DefaultProps

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types';
 // import React, { Fragment } from 'react';
 
 // FC
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
 
   return (
     <>
       <h1>{ saludo }</h1>
-      <p>Mi primera aplicación</p>
+      <p>{ subtitulo }</p>
     </>
   );
 }
 
 PrimeraApp.propTypes = {
   saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+  subtitulo: 'Mi primera aplicación'
 }
 
 export default PrimeraApp;
