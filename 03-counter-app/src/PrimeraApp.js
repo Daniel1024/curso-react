9680522e4f5d30fb783dc41cb1264f41da66b729
Daniel Lopez <d.lopez.1740@gmail.com>
2021-10-02 17:00:50 -0300

    38. Comunicación entre componentes - Props

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,9 +2,7 @@ import React from 'react';
 // import React, { Fragment } from 'react';
 
 // FC
-const PrimeraApp = () => {
-
-  const saludo = 'Hola Mundo const';
+const PrimeraApp = ({ saludo = 'Hola Mundo' }) => {
 
   return (
     <>
